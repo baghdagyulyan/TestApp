11bed04b25fff0c32d0551cf5bd2f001f27d1ad0
Davit Baghdagyulyan <dav.baghdagyulyan@gmail.com>
2018-12-06 01:40:23 +0400

    initial commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Davit Baghdagyulyan on 12/6/18.
+//  Copyright © 2018 Davit Baghdagyulyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
