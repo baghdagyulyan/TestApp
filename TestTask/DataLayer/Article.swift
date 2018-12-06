44de14907486d4d1cc8ae9a8f8887eb69cb822cb
Davit Baghdagyulyan <dav.baghdagyulyan@gmail.com>
2018-12-06 10:20:43 +0400

    Added class Article and class for Articles loading

---
+++ TestTask/DataLayer/Article.swift
@@ -0,0 +1,50 @@
+//
+//  Article.swift
+//  TestTask
+//
+//  Created by Davit Baghdagyulyan on 12/6/18.
+//  Copyright © 2018 Davit Baghdagyulyan. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+//class for main news type
+
+class Article: Object {
+    @objc private(set) dynamic var author: String?
+    @objc private(set) dynamic var title: String?
+    @objc private(set) dynamic var desc: String?
+    @objc private(set) dynamic var urlToImage: String?
+    @objc private(set) dynamic var content: String?
+    @objc private(set) dynamic var publicDate: String?
+    
+    convenience init(json: [String : Any]) {
+        self.init()
+        author = json["author"] as? String
+        title = json["title"] as? String
+        desc = json["description"] as? String
+        urlToImage = json["urlToImage"] as? String
+        content = json["content"] as? String
+        
+        let dateStr = json["publishedAt"] as? String
+        let dateFormatter = ISO8601DateFormatter()
+        let publishDate = dateFormatter.date(from:dateStr!)!
+        let currentDate = Date()
+        
+        let components = Calendar.current.dateComponents([.day, .hour, .minute], from: publishDate, to: currentDate)
+        if components.day! == 1 {
+            publicDate = "\(components.day!) day"
+        } else  if components.day! > 1 {
+            publicDate = "\(components.day!) days"
+        } else if components.hour! == 1 {
+            publicDate = "\(components.hour!) hour"
+        } else  if components.hour! > 1 {
+            publicDate = "\(components.hour!) hours"
+        } else  if components.minute! <= 1  {
+            publicDate = "\(components.minute!) minute"
+        } else {
+            publicDate = "\(components.minute!) minutes"
+        }
+    }
+}
---
+++ TestTask/DataLayer/ArticlesDataLoader.swift
@@ -0,0 +1,60 @@
+//
+//  ArticlesDataLoader.swift
+//  TestTask
+//
+//  Created by Davit Baghdagyulyan on 12/6/18.
+//  Copyright © 2018 Davit Baghdagyulyan. All rights reserved.
+//
+
+import Foundation
+
+typealias CompletionHandler = ([Article?]?, Error?) -> Void
+
+class ArticleDataLoader {
+    private let baseURL = "https://newsapi.org/v2/top-headlines"
+    private let apiKey = "2eef6ca9d9ac4fd6bca3bd7511a71513"
+    var articles = [Article?]()
+    private var url: String? {
+        get {
+            return String(format: "\(baseURL)?country=us&apiKey=\(apiKey)")
+        }
+    }
+    
+    // function for getting Articles array from json
+    static func newArticles(articles:[[String : AnyObject]]) -> [Article?] {
+        return articles.map{ element in
+            return Article.init(json: element)
+        }
+    }
+    
+    // session configuration load from cahce or not
+    static func configureSession() -> URLSession {
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        return URLSession.init(configuration: config)
+    }
+    
+    // load data from server
+    func loadDada(completion: @escaping CompletionHandler) {
+        DispatchQueue.global().async {
+            let requestUrl = URL(string: self.url!)
+            ArticleDataLoader.configureSession().dataTask(with: requestUrl!) {(data, response, error) in
+                guard let jsonData = data, error == nil else {
+                    completion(nil, error)
+                    return
+                }
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: AnyObject],
+                        let results = json["articles"] as? [[String:AnyObject]] {
+                        let items = ArticleDataLoader.newArticles(articles: results)
+                        completion(items, nil)
+                    } else {
+                        completion(nil, error)
+                    }
+                } catch {
+                    completion(nil, error)
+                }
+            }.resume()
+        }
+    }
+}
